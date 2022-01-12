4a7d3f2bdb7ea153958f77157d2c4af6ce3bcb28
sj <sebastianthegreatful@gmail.com>
2022-01-12 11:24:21 +0700

    refactor: deploy scripts accept group name (and uses new endpoint)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,11 @@
 HTTP="http --verify=no --check-status"
 
 INSTANCE_NAME=$1
+GROUP_NAME=$2
+
+GROUP_ID=$($HTTP --check-status "$INSTANCE_HOST/groups-name-to-id/$GROUP_NAME" "Authorization: Bearer $ACCESS_TOKEN")
 
 INSTANCE_ID=$($HTTP --check-status "$INSTANCE_HOST/instances-name-to-id/$GROUP_ID/$INSTANCE_NAME" "Authorization: Bearer $ACCESS_TOKEN")
-echo "INSTANCE_ID: $INSTANCE_ID"
 
 echo "{
   \"requiredParameters\": [
@@ -16,4 +18,4 @@ echo "{
       \"value\": \"0.5.0\"
     }
   ]
-}" | $HTTP post "$INSTANCE_HOST/instances/$INSTANCE_ID/launch" "Authorization: Bearer $ACCESS_TOKEN"
+}" | $HTTP post "$INSTANCE_HOST/instances/$INSTANCE_ID/deploy" "Authorization: Bearer $ACCESS_TOKEN"
