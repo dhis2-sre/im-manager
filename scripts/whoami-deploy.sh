68f1ba69ad524798f6f7c7af17d4f93e581dea5e
sj <sebastianthegreatful@gmail.com>
2022-02-21 15:11:42 +0700

    feat: scripts for creating and deploying a whoami-go instance

---
+++ scripts/whoami-deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+INSTANCE_NAME=$1
+GROUP_NAME=$2
+
+GROUP_ID=$($HTTP --check-status "$INSTANCE_HOST/groups-name-to-id/$GROUP_NAME" "Authorization: Bearer $ACCESS_TOKEN")
+INSTANCE_ID=$($HTTP --check-status "$INSTANCE_HOST/instances-name-to-id/$GROUP_ID/$INSTANCE_NAME" "Authorization: Bearer $ACCESS_TOKEN")
+
+echo "{
+  \"requiredParameters\": [
+    {
+      \"stackParameterId\": 1,
+      \"value\": \"0.5.0\"
+    }
+  ]
+}" | $HTTP post "$INSTANCE_HOST/instances/$INSTANCE_ID/deploy" "Authorization: Bearer $ACCESS_TOKEN"
