c85f72bda605961229eafc9cd918d06a26f3123b
sj <sebastianthegreatful@gmail.com>
2022-02-21 15:08:02 +0700

    feat: script for finding an instance by its name (and group name)

---
+++ scripts/findByName.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+INSTANCE_NAME=$1
+GROUP_NAME=$2
+
+GROUP_ID=$($HTTP --check-status "$INSTANCE_HOST/groups-name-to-id/$GROUP_NAME" "Authorization: Bearer $ACCESS_TOKEN")
+INSTANCE_ID=$($HTTP --check-status "$INSTANCE_HOST/instances-name-to-id/$GROUP_ID/$INSTANCE_NAME" "Authorization: Bearer $ACCESS_TOKEN")
+
+$HTTP "$INSTANCE_HOST/instances/$INSTANCE_ID" "Authorization: Bearer $ACCESS_TOKEN"
