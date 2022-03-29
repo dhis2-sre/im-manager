3bef5f5612dd9c8dac6156adbc74d189b14af86e
sj <sebastianthegreatful@gmail.com>
2022-03-29 17:00:42 +0700

    chore: "revert" previous commit, so it's only deployed to prod

--- stacks/dhis2/seed.sh
+++ stacks/dhis2/seed.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 if [ -n "$DATABASE_ID" ]; then
 #  ABSOLUTE_SEED_URL="https://databases.dhis2.org/sierra-leone/$SEED_URL"
 #  ABSOLUTE_SEED_URL="$DATABASE_MANAGER_SERVICE_HOST/$DATABASE_MANAGER_SERVICE_BASE_PATH/databases/$DATABASE_ID/download"
-  ABSOLUTE_SEED_URL="im-database-manager-prod.instance-manager-prod.svc:8080/databases/$DATABASE_ID/download"
+  ABSOLUTE_SEED_URL="im-database-manager-dev.instance-manager-dev.svc:8080/databases/$DATABASE_ID/download"
   echo "DATABASE_HOST: $ABSOLUTE_SEED_URL"
   curl --fail -H "Authorization: $IM_ACCESS_TOKEN" -L "$ABSOLUTE_SEED_URL" -o /tmp/t$$ | cat
   gunzip -c /tmp/t$$ > /tmp/t$$-seed-data
