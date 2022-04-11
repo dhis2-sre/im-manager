050363b4e9ae4d51f6ee14fdedbdfefe5171a203
radnov <radnov@pm.me>
2022-04-11 16:09:58 +0300

    fix: Use func names with argument signatures to change ownership [DEVOPS-102]

--- stacks/dhis2/seed.sh
+++ stacks/dhis2/seed.sh
@@ -52,11 +52,9 @@ if [ -n "$DATABASE_ID" ]; then
   done
 
   # Functions
-#  functions=$(psql -U postgres -qAt -c "select routine_name from information_schema.routines where routine_type = 'FUNCTION' and  routine_schema = 'public'" "$DATABASE_NAME")
-#  for function in $functions; do
-#      echo "Changing owner of $function to $DATABASE_USERNAME"
-#      psql -U postgres -c "alter function \"$function\" owner to $DATABASE_USERNAME" "$DATABASE_NAME"
-#  done
+  psql -U postgres -c -qAt "select 'alter function '||p.proname||'('||pg_get_function_identity_arguments(p.oid)||') owner to dhis;' from pg_proc p join pg_namespace nsp ON p.pronamespace = nsp.oid where nsp.nspname = 'public'" > functions.sql
+  echo "Changing owner of functions to $DATABASE_USERNAME"
+  psql -U postgres -f functions.sql
 
 else
   psql -U postgres -d "$DATABASE_USERNAME" -p 5432 -c "create extension if not exists postgis"
