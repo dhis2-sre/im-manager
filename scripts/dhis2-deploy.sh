3f506c7b1a3448f58fbb4164c3ec27eb5855eb85
sj <sebastianthegreatful@gmail.com>
2022-04-29 10:07:10 +0700

    refactor: list parameter id along with their values for ease of updating

--- scripts/dhis2-deploy.sh
+++ scripts/dhis2-deploy.sh
@@ -2,8 +2,23 @@
 
 set -euo pipefail
 
+DATABASE_SIZE="30Gi"
+DATABASE_SIZE_PARAMETER_ID=10
+
+READINESS_PROBE_INITIAL_DELAY_SECONDS=100
+READINESS_PROBE_INITIAL_DELAY_SECONDS_PARAMETER_ID=12
+
+LIVENESS_PROBE_INITIAL_DELAY_SECONDS=100
+LIVENESS_PROBE_INITIAL_DELAY_SECONDS_PARAMETER_ID=5
+
+IMAGE_REPOSITORY=core
+IMAGE_REPOSITORY_PARAMETER_ID=11
+
 IMAGE_TAG="2.36.0-tomcat-8.5.34-jre8-alpine"
-SEED_PATH="2.36.0/dhis2-db-sierra-leone.sql.gz"
+IMAGE_TAG_PARAMETER_ID=1
+
+DATABASE_ID=1
+DATABASE_ID_PARAMETER_ID=1
 
 INSTANCE_NAME=$1
 GROUP_NAME=$2
@@ -17,12 +32,30 @@ echo "{
   \"stackId\": 1,
   \"optionalParameters\": [
     {
-      \"stackParameterId\": 3,
-      \"value\": \"$SEED_PATH\"
+      \"stackParameterId\": $READINESS_PROBE_INITIAL_DELAY_SECONDS_PARAMETER_ID,
+      \"value\": \"$READINESS_PROBE_INITIAL_DELAY_SECONDS\"
     },
     {
-      \"stackParameterId\": 1,
+      \"stackParameterId\": $LIVENESS_PROBE_INITIAL_DELAY_SECONDS_PARAMETER_ID,
+      \"value\": \"$LIVENESS_PROBE_INITIAL_DELAY_SECONDS\"
+    },
+    {
+      \"stackParameterId\": $IMAGE_REPOSITORY_PARAMETER_ID,
+      \"value\": \"$IMAGE_REPOSITORY\"
+    },
+    {
+      \"stackParameterId\": $IMAGE_TAG_PARAMETER_ID,
       \"value\": \"$IMAGE_TAG\"
+    },
+    {
+      \"stackParameterId\": $DATABASE_SIZE_PARAMETER_ID,
+      \"value\": \"$DATABASE_SIZE\"
+    }
+  ],
+  \"requiredParameters\": [
+    {
+      \"stackParameterId\": $DATABASE_ID_PARAMETER_ID,
+      \"value\": \"$DATABASE_ID\"
     }
   ]
 }" | $HTTP post "$INSTANCE_HOST/instances/$INSTANCE_ID/deploy" "Authorization: Bearer $ACCESS_TOKEN"
