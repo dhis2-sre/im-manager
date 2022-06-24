9a5b034ceea068ee720045179381b0770683c98c
sj <sebastianthegreatful@gmail.com>
2022-06-25 03:06:54 +0700

    feat: whoami deploy script with TTL parameter

---
+++ scripts/whoami-deploy-with-ttl.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+CHART_VERSION="0.5.0"
+
+INSTANCE_NAME=$1
+GROUP_NAME=$2
+INSTANCE_TTL=${3-10}
+
+INSTANCE_ID=$($HTTP --check-status "$INSTANCE_HOST/instances-name-to-id/$GROUP_NAME/$INSTANCE_NAME" "Authorization: Bearer $ACCESS_TOKEN")
+
+echo "{
+  \"requiredParameters\": [
+    {
+      \"stackParameter\": \"CHART_VERSION\",
+      \"value\": \"$CHART_VERSION\"
+    }
+  ],
+  \"optionalParameters\": [
+    {
+       \"stackParameter\": \"INSTANCE_TTL\",
+       \"value\": \"$INSTANCE_TTL\"
+    }
+  ]
+}" | $HTTP post "$INSTANCE_HOST/instances/$INSTANCE_ID/deploy" "Authorization: Bearer $ACCESS_TOKEN"
