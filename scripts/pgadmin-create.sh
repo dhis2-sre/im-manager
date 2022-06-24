01025636ba4cec53f04539091cd8ea946922592c
sj <sebastianthegreatful@gmail.com>
2022-06-24 14:19:45 +0700

    feat: pgadmin

---
+++ scripts/pgadmin-create.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+STACK_NAME=pgadmin
+
+NAME=$1
+GROUP_NAME=$2
+
+echo "{
+  \"name\": \"$NAME\",
+  \"groupName\": \"$GROUP_NAME\",
+  \"stackName\": \"$STACK_NAME\"
+}" | $HTTP post "$INSTANCE_HOST/instances" "Authorization: Bearer $ACCESS_TOKEN"
