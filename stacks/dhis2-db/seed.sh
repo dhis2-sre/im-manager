b5ac9936f6cd1108db123f9b55aa520f6314aafa
sj <sebastianthegreatful@gmail.com>
2022-06-25 01:03:52 +0700

    refactor: abort seeding if database is already seeded

--- stacks/dhis2-db/seed.sh
+++ stacks/dhis2-db/seed.sh
@@ -5,6 +5,15 @@ set -euo pipefail
 psql -U postgres -qAt -d "$DATABASE_NAME" -c "create extension if not exists postgis"
 
 if [ -n "$DATABASE_ID" ]; then
+  table_exists=$(psql -U postgres -qAt -c "select exists (select from information_schema.tables where table_schema = 'schema_name' and table_name = 'table_name')" "$DATABASE_NAME")
+  if [ "$table_exists" = "true" ]; then
+    row_count=$(psql -U postgres -qAt -c "select count(*) from organisationunit" "$DATABASE_NAME")
+    if [ $row_count -ne 0 ]; then
+      echo "Seeding aborted!"
+      exit 0
+    fi
+  fi
+
   DATABASE_MANAGER_ABSOLUTE_URL="$DATABASE_MANAGER_URL:8080/databases/$DATABASE_ID/download"
   echo "DATABASE_MANAGER_ABSOLUTE_URL: $DATABASE_MANAGER_ABSOLUTE_URL"
 
