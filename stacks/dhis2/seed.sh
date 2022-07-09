e690650e559dba2c50041cf6d782c2010bfe7792
sj <sebastianthegreatful@gmail.com>
2022-07-09 16:56:14 +0700

    refactor: use exec_psql

--- stacks/dhis2/seed.sh
+++ stacks/dhis2/seed.sh
@@ -30,21 +30,21 @@ if [ -n "$DATABASE_ID" ]; then
   entities=$(exec_psql "select tablename from pg_tables where schemaname = 'public'")
   for entity in $entities; do
     echo "Changing owner of $entity to $DATABASE_USERNAME"
-    psql -U postgres -c "alter table \"$entity\" owner to $DATABASE_USERNAME" "$DATABASE_NAME"
+    exec_psql "alter table \"$entity\" owner to $DATABASE_USERNAME"
   done
 
   # Sequences
   entities=$(exec_psql "select sequence_name from information_schema.sequences where sequence_schema = 'public'")
   for entity in $entities; do
     echo "Changing owner of $entity to $DATABASE_USERNAME"
-    psql -U postgres -c "alter sequence \"$entity\" owner to $DATABASE_USERNAME" "$DATABASE_NAME"
+    exec_psql "alter sequence \"$entity\" owner to $DATABASE_USERNAME"
   done
 
   # Views
   entities=$(exec_psql "select table_name from information_schema.views where table_schema = 'public'")
   for entity in $entities; do
     echo "Changing owner of $entity to $DATABASE_USERNAME"
-    psql -U postgres -c "alter view \"$entity\" owner to $DATABASE_USERNAME" "$DATABASE_NAME"
+    exec_psql "alter view \"$entity\" owner to $DATABASE_USERNAME"
   done
 
 fi
