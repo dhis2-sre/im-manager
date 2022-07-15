aef64f3cb5c57874a7debf2e38ef10d0844ab9f5
teleivo <teleivo@users.noreply.github.com>
2022-07-15 12:11:50 +0200

    chore: use startup probe for slow DHIS2 startup DEVOPS-180 (#66)
    * chore: startupProbe replaces need for initialDelay
    
    use startupProbe with ~2min total as it takes ~2min for DHIS2 to finish
    its startup if the DB can or cannot be accessed. initialDelaySeconds has
    been removed from the chart as liveness/readiness probes now only run
    after the startup probe succeeded and is 0 by default in kubernetes.
    
    * chore: add startup probe params to deploy script
    
    so we can easily iterate on the total startup probe time if the current
    default of 130s does not turn out to be good
    
    * chore: adapt dhis2-core stack as well

--- scripts/dhis2-core-deploy.sh
+++ scripts/dhis2-core-deploy.sh
@@ -2,9 +2,11 @@
 
 set -euo pipefail
 
+# container(s) in dhis2 pod will be restarted after that due to restartPolicy
+# 5*26=130s
+STARTUP_PROBE_FAILURE_THRESHOLD=26
+STARTUP_PROBE_PERIOD_SECONDS=5
 IMAGE_TAG=2.36.0-tomcat-8.5.34-jre8-alpine
-READINESS_PROBE_INITIAL_DELAY_SECONDS=100
-LIVENESS_PROBE_INITIAL_DELAY_SECONDS=100
 
 INSTANCE_NAME=$1
 GROUP_NAME=$2
@@ -18,12 +20,12 @@ echo "{
   \"stackName\": \"$STACK_NAME\",
   \"optionalParameters\": [
     {
-      \"name\": \"READINESS_PROBE_INITIAL_DELAY_SECONDS\",
-      \"value\": \"$READINESS_PROBE_INITIAL_DELAY_SECONDS\"
+      \"name\": \"STARTUP_PROBE_FAILURE_THRESHOLD\",
+      \"value\": \"$STARTUP_PROBE_FAILURE_THRESHOLD\"
     },
     {
-      \"name\": \"LIVENESS_PROBE_INITIAL_DELAY_SECONDS\",
-      \"value\": \"$LIVENESS_PROBE_INITIAL_DELAY_SECONDS\"
+      \"name\": \"STARTUP_PROBE_PERIOD_SECONDS\",
+      \"value\": \"$STARTUP_PROBE_PERIOD_SECONDS\"
     },
     {
       \"name\": \"IMAGE_TAG\",
