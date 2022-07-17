3136e4f0ce0398ef1b9d2a86ff0bf3e6e658b336
tonsV2 <tonsV2@users.noreply.github.com>
2022-07-17 11:03:09 +0700

    Fix/missing stack parameters (#67)
    fix: handle non existing stack parameters file

--- pkg/instance/helmfile.go
+++ pkg/instance/helmfile.go
@@ -1,6 +1,7 @@
 package instance
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -69,13 +70,16 @@ func (h helmfileService) executeHelmfileCommand(accessToken string, instance *mo
 	return cmd, nil
 }
 
-type StackParameters map[string]string
+type stackParameters map[string]string
 
-func (h helmfileService) loadStackParameters(folder string, name string) (StackParameters, error) {
+func (h helmfileService) loadStackParameters(folder string, name string) (stackParameters, error) {
 	environment := h.config.Environment
 	path := fmt.Sprintf("%s/%s/parameters/%s/parameters.yaml", folder, name, environment)
 	data, err := os.ReadFile(path)
 	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil, nil
+		}
 		return nil, err
 	}
 
@@ -84,7 +88,7 @@ func (h helmfileService) loadStackParameters(folder string, name string) (StackP
 		return nil, err
 	}
 
-	var params StackParameters
+	var params stackParameters
 	err = yaml.Unmarshal(b, &params)
 	if err != nil {
 		return nil, err
@@ -93,7 +97,7 @@ func (h helmfileService) loadStackParameters(folder string, name string) (StackP
 	return params, nil
 }
 
-func configureInstanceEnvironment(accessToken string, instance *model.Instance, group *models.Group, stackParameters StackParameters, cmd *exec.Cmd) {
+func configureInstanceEnvironment(accessToken string, instance *model.Instance, group *models.Group, stackParameters stackParameters, cmd *exec.Cmd) {
 	// TODO: We should only inject what the stack require, currently we just blindly inject IM_ACCESS_TOKEN and others which may not be required by the stack
 	// We could probably list the required system parameters in the stacks helmfile and parse those as well as other parameters
 	instanceNameEnv := fmt.Sprintf("%s=%s", "INSTANCE_NAME", instance.Name)
