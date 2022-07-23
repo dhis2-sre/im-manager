1f6cfb2f7ff761312a9e21c2a9fd00ec4c759090
teleivo <teleivo@users.noreply.github.com>
2022-07-23 15:57:13 +0200

    chore(wip): implement env with default

--- pkg/stack/parser.go
+++ pkg/stack/parser.go
@@ -16,14 +16,19 @@ import (
 
 type tmpl struct {
 	requiredEnvs []string
+	envs         map[string]any
 }
 
 // TODO pass in name of stack? for better error message
 func parse(in string) (*tmpl, error) {
-	result := &tmpl{}
+	result := &tmpl{
+		envs: make(map[string]any),
+	}
 
 	t, err := template.New("").Funcs(template.FuncMap{
 		"requiredEnv": requiredEnv(result),
+		"env":         env(result),
+		"default":     dfault(result),
 	}).Parse(in)
 	if err != nil {
 		return nil, err
@@ -37,7 +42,7 @@ func parse(in string) (*tmpl, error) {
 	return result, err
 }
 
-// requiredEnv replaces the helmfile requiredEnv template function. It ensures stack templates
+// requiredEnv replaces the helmfile requiredEnv template function. This implementation ensures stack templates
 // calling requiredEnv provide one arg of type string which is not blank.
 // https://github.com/helmfile/helmfile/blob/70d2dd653b5fd7a64d834aa99e07d727d3f4d10d/pkg/tmpl/context_funcs.go#L313
 func requiredEnv(result *tmpl) func(string) (string, error) {
@@ -46,6 +51,37 @@ func requiredEnv(result *tmpl) func(string) (string, error) {
 			return "", errors.New("must provide name")
 		}
 		result.requiredEnvs = append(result.requiredEnvs, name)
+
+		return name, nil
+	}
+}
+
+// env replaces the Sprig env template function which is actually os.Getenv. This implementation ensures stack templates
+// calling env provide one arg of type string which is not blank.
+// https://github.com/Masterminds/sprig/blob/5a09ebddb9c54ae8c3531154a6e16b93b87c47a8/functions.go#L267
+func env(result *tmpl) func(string) (string, error) {
+	return func(name string) (string, error) {
+		if strings.TrimSpace(name) == "" {
+			return "", errors.New("must provide name")
+		}
+		result.envs[name] = ""
+
+		return name, nil
+	}
+}
+
+// dfault replaces the Sprig default template function. This implementation ensures stack templates
+// calling default provide two args. It expects a string as a second argument as we only use it to
+// default environment variables retrieved via function env which are not empty or not set.
+// https://github.com/Masterminds/sprig/blob/3ac42c7bc5e4be6aa534e036fb19dde4a996da2e/defaults.go#L26
+func dfault(result *tmpl) func(any, string) (string, error) {
+	return func(d any, name string) (string, error) {
+		if strings.TrimSpace(name) == "" {
+			return "", errors.New("must provide name")
+		}
+		// TODO should we check the default value in any way? It will be provided otherwise the
+		// text/template Execute will fail
+		result.envs[name] = d
 
 		return name, nil
 	}
--- pkg/stack/parser_test.go
+++ pkg/stack/parser_test.go
@@ -59,3 +59,70 @@ func TestParserRequiredEnv(t *testing.T) {
 		})
 	}
 }
+
+func TestParserEnv(t *testing.T) {
+	tt := map[string]struct {
+		in   string
+		want map[string]any
+	}{
+		"Success": {
+			in: `{{env "INSTANCE_NAME"}}`,
+			want: map[string]any{
+				"INSTANCE_NAME": "",
+			},
+		},
+		"SuccessWithDefaultString": {
+			in: `{{env "INSTANCE_NAME" | default "DHIS2"}}`,
+			want: map[string]any{
+				"INSTANCE_NAME": "DHIS2",
+			},
+		},
+		"SuccessWithDefaultNumber": {
+			in: `{{env "CHART_VERSION" | default 2}}`,
+			want: map[string]any{
+				"CHART_VERSION": 2,
+			},
+		},
+	}
+
+	for n, tt := range tt {
+		t.Run(n, func(t *testing.T) {
+			assert := assert.New(t)
+			require := require.New(t)
+
+			tmpl, err := parse(tt.in)
+
+			require.NoError(err)
+			assert.Equal(tt.want, tmpl.envs)
+		})
+	}
+
+	te := map[string]struct {
+		in      string
+		wantErr string
+	}{
+		"MissingEnv": {
+			in: `{{requiredEnv}}`,
+		},
+		"BlankEnv": {
+			in:      `{{requiredEnv "   "}}`,
+			wantErr: "must provide name",
+		},
+	}
+
+	for n, te := range te {
+		t.Run(n, func(t *testing.T) {
+			assert := assert.New(t)
+			require := require.New(t)
+
+			tmpl, err := parse(te.in)
+
+			assert.Nil(tmpl)
+			if te.wantErr != "" {
+				require.ErrorContains(err, te.wantErr)
+			} else {
+				require.Error(err)
+			}
+		})
+	}
+}
