db9eedbd921d343456109393372d84d8b015313d
TonsV2 <sebastianthegreatful@gmail.com>
2022-07-30 02:11:04 +0700

    chore: remove unused docs code

--- pkg/instance/docs.go
+++ pkg/instance/docs.go
@@ -27,22 +27,6 @@ type _ struct {
 	ID uint `json:"id"`
 }
 
-// swagger:parameters linkDeployInstance
-type _ struct {
-	// in: path
-	// required: true
-	ID uint `json:"id"`
-
-	// in: path
-	// required: true
-	DestinationId uint `json:"destinationId"`
-
-	// Deploy instance request body parameter
-	// in: body
-	// required: true
-	_ DeployInstanceRequest
-}
-
 // swagger:parameters instanceLogs
 type _ struct {
 	// in: path
