0ec343789d2cc22ac2e40a0ddb5862f4b3b63413
TonsV2 <sebastianthegreatful@gmail.com>
2022-09-17 12:11:20 +0700

    refactor: reuse scripts

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -8,7 +8,7 @@ GROUP=$1
 NAME=$2
 
 function int_handler {
-  $HTTP delete "$INSTANCE_HOST/instances/$INSTANCE_ID" "Authorization: Bearer $ACCESS_TOKEN"
+  ./destroy.sh $GROUP $NAME
   trap - EXIT
   exit
 }
@@ -16,25 +16,10 @@ function int_handler {
 trap int_handler INT
 trap int_handler EXIT
 
-# Deploy instance
-INSTANCE_OUTPUT=$(echo "{
-  \"name\": \"$NAME\",
-  \"groupName\": \"$GROUP\",
-  \"stackName\": \"$STACK\",
-  \"requiredParameters\": [
-    {
-      \"name\": \"CHART_VERSION\",
-      \"value\": \"$CHART_VERSION\"
-    }
-  ]
-}" | $HTTP post "$INSTANCE_HOST/instances" "Authorization: Bearer $ACCESS_TOKEN")
+./deploy-whoami.sh $GROUP $NAME
 
-INSTANCE_ID=$(echo "$INSTANCE_OUTPUT" | jq -r '.ID')
-
-# Show created instance
-$HTTP "$INSTANCE_HOST/instances/$INSTANCE_ID" "Authorization: Bearer $ACCESS_TOKEN"
+./findByName.sh $GROUP $NAME
 
 sleep 5
 
-# Stream instance logs
-$HTTP --stream "$INSTANCE_HOST/instances/$INSTANCE_ID/logs" "Authorization: Bearer $ACCESS_TOKEN"
+./logs.sh $GROUP $NAME
