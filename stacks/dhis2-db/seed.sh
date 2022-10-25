c287db28630df347b8fd06390205b5c440787279
TonsV2 <sebastianthegreatful@gmail.com>
2022-10-25 12:47:28 +0700

    chore: pg_restore can return a nonzero return code on some benign errors which will cause gunzip to fail... Thus "|| true" (which isn't ideal)

--- stacks/dhis2-db/seed.sh
+++ stacks/dhis2-db/seed.sh
@@ -26,8 +26,10 @@ if [[ -n $DATABASE_ID ]]; then
   tmp_file=$(mktemp)
   curl --connect-timeout 10 --retry 5 --retry-delay 1 --fail -L "$DATABASE_MANAGER_ABSOLUTE_URL" -H "Authorization: $IM_ACCESS_TOKEN" > "$tmp_file"
 # Try pg_restore... Or gzipped sql
-  (pg_restore -U postgres -d "$DATABASE_NAME" -j 4 "$tmp_file") || \
-  (gunzip -v -c "$tmp_file" | psql -U postgres -d "$DATABASE_NAME")
+# pg_restore often returns a non zero return code due to benign errors resulting in executing of gunzip despite the restore being successful
+# gunzip will fail because the input isn't gzipped causing the whole seed script to fail... Which is why there's a "|| true" at the end
+  (pg_restore --verbose --create -U postgres -d "$DATABASE_NAME" -j 4 "$tmp_file") || \
+  (gunzip -v -c "$tmp_file" | psql -U postgres -d "$DATABASE_NAME") || true
   rm "$tmp_file"
 
   ## Change ownership to $DATABASE_USERNAME
