bf72a6e3e0c6b489782dfb1473e3f82f256a9eac
TonsV2 <sebastianthegreatful@gmail.com>
2022-11-01 16:53:59 +0700

    chore: implement update-whoami.sh

---
+++ scripts/update-whoami.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source ./auth.sh
+
+GROUP=$1
+NAME=$2
+CHART_VERSION=${CHART_VERSION:-0.9.0}
+IMAGE_TAG=${IMAGE_TAG:-0.6.0}
+INSTANCE_TTL=${INSTANCE_TTL:-""}
+
+INSTANCE_ID=$($HTTP --check-status "$INSTANCE_HOST/instances-name-to-id/$GROUP/$NAME" "Authorization: Bearer $ACCESS_TOKEN")
+
+echo "{
+  \"optionalParameters\": [
+    {
+      \"name\": \"CHART_VERSION\",
+      \"value\": \"$CHART_VERSION\"
+    },
+    {
+      \"name\": \"IMAGE_TAG\",
+      \"value\": \"$IMAGE_TAG\"
+    },
+    {
+      \"name\": \"INSTANCE_TTL\",
+      \"value\": \"$INSTANCE_TTL\"
+    }
+  ]
+}" | $HTTP put "$INSTANCE_HOST/instances/$INSTANCE_ID" "Authorization: Bearer $ACCESS_TOKEN"
