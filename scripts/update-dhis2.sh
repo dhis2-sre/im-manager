df57365b6b6791d1f2b8e2da92ff3f1a43019d5d
radnov <radnov@pm.me>
2022-11-01 11:10:59 +0200

    feat: Add user script for updating dhis2 stack instances [DEVOPS-208]

---
+++ scripts/update-dhis2.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source ./auth.sh
+
+STACK=dhis2
+
+GROUP=$1
+NAME=$2
+
+# container(s) in dhis2 pod will be restarted after that due to restartPolicy
+# 5*26=130s
+STARTUP_PROBE_FAILURE_THRESHOLD=${STARTUP_PROBE_FAILURE_THRESHOLD:-26}
+STARTUP_PROBE_PERIOD_SECONDS=${STARTUP_PROBE_PERIOD_SECONDS:-5}
+IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-core}
+IMAGE_TAG=${IMAGE_TAG:-2.39.0}
+IMAGE_PULL_POLICY=${IMAGE_PULL_POLICY:-IfNotPresent}
+DATABASE_SIZE=${DATABASE_SIZE:-30Gi}
+PGADMIN_INSTALL=${PGADMIN_INSTALL:-false}
+DATABASE_ID=${DATABASE_ID:-1}
+INSTANCE_TTL=${INSTANCE_TTL:-""}
+
+INSTANCE_ID=$($HTTP --check-status "$INSTANCE_HOST/instances-name-to-id/$GROUP/$NAME" "Authorization: Bearer $ACCESS_TOKEN")
+
+echo "{
+  \"optionalParameters\": [
+    {
+      \"name\": \"STARTUP_PROBE_FAILURE_THRESHOLD\",
+      \"value\": \"$STARTUP_PROBE_FAILURE_THRESHOLD\"
+    },
+    {
+      \"name\": \"STARTUP_PROBE_PERIOD_SECONDS\",
+      \"value\": \"$STARTUP_PROBE_PERIOD_SECONDS\"
+    },
+    {
+      \"name\": \"IMAGE_REPOSITORY\",
+      \"value\": \"$IMAGE_REPOSITORY\"
+    },
+    {
+      \"name\": \"IMAGE_TAG\",
+      \"value\": \"$IMAGE_TAG\"
+    },
+    {
+      \"name\": \"IMAGE_PULL_POLICY\",
+      \"value\": \"$IMAGE_PULL_POLICY\"
+    },
+    {
+      \"name\": \"DATABASE_SIZE\",
+      \"value\": \"$DATABASE_SIZE\"
+    },
+    {
+      \"name\": \"PGADMIN_INSTALL\",
+      \"value\": \"$PGADMIN_INSTALL\"
+    },
+    {
+       \"name\": \"INSTANCE_TTL\",
+       \"value\": \"$INSTANCE_TTL\"
+    }
+  ],
+  \"requiredParameters\": [
+    {
+      \"name\": \"DATABASE_ID\",
+      \"value\": \"$DATABASE_ID\"
+    }
+  ]
+}" | $HTTP put "$INSTANCE_HOST/instances/$INSTANCE_ID" "Authorization: Bearer $ACCESS_TOKEN"
