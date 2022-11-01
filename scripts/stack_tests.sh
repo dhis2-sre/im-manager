b4347afa53a77bbcea9dda544ecd60cc12e1c5e7
tonsV2 <tonsV2@users.noreply.github.com>
2022-11-01 17:25:09 +0700

    Feat/seed big db (#117)
    * chore: only create extensions if we seed
    
    * chore: add retry and timeout when connecting to the database manager
    
    * chore: pg_restore can return a nonzero return code on some benign errors which will cause gunzip to fail... Thus "|| true" (which isn't ideal)
    
    * chore: run stack tests periodically on push and PRs

---
+++ scripts/stack_tests.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+INSTANCE_HOST_DEPLOY=https://whoami.im.dev.test.c.dhis2.org
+GROUP=whoami
+
+# Whoami
+INSTANCE_NAME=whoami-test
+./deploy-whoami.sh $GROUP $INSTANCE_NAME
+sleep 3
+kubectl wait --for=condition=available --timeout=30s --namespace $GROUP deployment/$INSTANCE_NAME-whoami-go
+http --check-status "$INSTANCE_HOST_DEPLOY/$INSTANCE_NAME"
+./destroy.sh $GROUP $INSTANCE_NAME
+
+# Monolith
+INSTANCE_NAME=monolith-test
+./deploy-dhis2.sh $GROUP $INSTANCE_NAME
+sleep 3
+kubectl wait --for=condition=available --timeout=180s --namespace $GROUP deployment/$INSTANCE_NAME-core
+http --check-status --follow "$INSTANCE_HOST_DEPLOY/$INSTANCE_NAME"
+./destroy.sh $GROUP $INSTANCE_NAME
+kubectl delete pvc --namespace $GROUP data-$INSTANCE_NAME-database-postgresql-0
+
+# Database and core
+INSTANCE_NAME=db-and-core-test
+./deploy-dhis2-db.sh $GROUP $INSTANCE_NAME
+./deploy-dhis2-core.sh $GROUP $INSTANCE_NAME $INSTANCE_NAME-core
+sleep 3
+kubectl rollout status --watch --timeout=300s --namespace $GROUP statefulset/$INSTANCE_NAME-database-postgresql
+kubectl wait --for=condition=available --timeout=300s --namespace $GROUP deployment/$INSTANCE_NAME-core
+sleep 3
+http --check-status --follow "$INSTANCE_HOST_DEPLOY/$INSTANCE_NAME-core"
+./destroy.sh $GROUP "$INSTANCE_NAME-core"
+./destroy.sh $GROUP "$INSTANCE_NAME"
+kubectl delete pvc --namespace $GROUP data-$INSTANCE_NAME-database-postgresql-0
--- stacks/dhis2-db/seed.sh
+++ stacks/dhis2-db/seed.sh
@@ -6,10 +6,6 @@ function exec_psql() {
   PGPASSWORD=$POSTGRES_POSTGRES_PASSWORD psql -U postgres -qAt -d "$DATABASE_NAME" -c "$1"
 }
 
-exec_psql "create extension if not exists postgis"
-exec_psql "create extension if not exists pg_trgm"
-exec_psql "create extension if not exists btree_gin"
-
 if [[ -n $DATABASE_ID ]]; then
   table_exists=$(exec_psql "select exists (select from information_schema.tables where table_schema = 'schema_name' and table_name = 'table_name')")
   if [[ $table_exists = true ]]; then
@@ -23,11 +19,17 @@ if [[ -n $DATABASE_ID ]]; then
   DATABASE_MANAGER_ABSOLUTE_URL="$DATABASE_MANAGER_URL/databases/$DATABASE_ID/download"
   echo "DATABASE_MANAGER_ABSOLUTE_URL: $DATABASE_MANAGER_ABSOLUTE_URL"
 
+  exec_psql "create extension if not exists postgis"
+  exec_psql "create extension if not exists pg_trgm"
+  exec_psql "create extension if not exists btree_gin"
+
   tmp_file=$(mktemp)
-  curl --fail -L "$DATABASE_MANAGER_ABSOLUTE_URL" -H "Authorization: $IM_ACCESS_TOKEN" > "$tmp_file"
+  curl --connect-timeout 10 --retry 5 --retry-delay 1 --fail -L "$DATABASE_MANAGER_ABSOLUTE_URL" -H "Authorization: $IM_ACCESS_TOKEN" > "$tmp_file"
 # Try pg_restore... Or gzipped sql
-  (pg_restore -U postgres -d "$DATABASE_NAME" -j 4 "$tmp_file") || \
-  (gunzip -v -c "$tmp_file" | psql -U postgres -d "$DATABASE_NAME")
+# pg_restore often returns a non zero return code due to benign errors resulting in executing of gunzip despite the restore being successful
+# gunzip will fail because the input isn't gzipped causing the whole seed script to fail... Which is why there's a "|| true" at the end
+  (pg_restore --verbose --create -U postgres -d "$DATABASE_NAME" -j 4 "$tmp_file") || \
+  (gunzip -v -c "$tmp_file" | psql -U postgres -d "$DATABASE_NAME") || true
   rm "$tmp_file"
 
   ## Change ownership to $DATABASE_USERNAME
