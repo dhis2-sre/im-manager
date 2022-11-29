f3d77169d0caabd48823718252d11d9155107d23
TonsV2 <sebastianthegreatful@gmail.com>
2022-11-29 14:22:08 +0700

    chore: increase timeouts and "unroll" destroy.sh

--- scripts/stack_tests.sh
+++ scripts/stack_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -xeuo pipefail
+set -euo pipefail
 
 INSTANCE_HOST_DEPLOY=https://whoami.im.dev.test.c.dhis2.org
 GROUP=whoami
@@ -29,7 +29,7 @@ http --check-status "$INSTANCE_HOST_DEPLOY/$INSTANCE_NAME"
 INSTANCE_NAME=test-monolith
 ./deploy-dhis2.sh $GROUP $INSTANCE_NAME
 sleep 3
-kubectl wait --for=condition=available --timeout=300s --namespace $GROUP deployment/$INSTANCE_NAME-core
+kubectl wait --for=condition=available --timeout=600s --namespace $GROUP deployment/$INSTANCE_NAME-core
 sleep 3
 http --check-status --follow "$INSTANCE_HOST_DEPLOY/$INSTANCE_NAME"
 ./destroy.sh $GROUP $INSTANCE_NAME
@@ -38,10 +38,10 @@ kubectl delete pvc --namespace $GROUP data-$INSTANCE_NAME-database-postgresql-0
 # Database and core
 INSTANCE_NAME=test-db-and-core
 ./deploy-dhis2-db.sh $GROUP $INSTANCE_NAME
-./deploy-dhis2-core.sh $GROUP $INSTANCE_NAME $INSTANCE_NAME-core
+./deploy-dhis2-core.sh $GROUP $INSTANCE_NAME-core $INSTANCE_NAME
 sleep 3
-kubectl rollout status --watch --timeout=300s --namespace $GROUP statefulset/$INSTANCE_NAME-database-postgresql
-kubectl wait --for=condition=available --timeout=300s --namespace $GROUP deployment/$INSTANCE_NAME-core
+kubectl rollout status --watch --timeout=600s --namespace $GROUP statefulset/$INSTANCE_NAME-database-postgresql
+kubectl wait --for=condition=available --timeout=600s --namespace $GROUP deployment/$INSTANCE_NAME-core
 sleep 3
 http --check-status --follow "$INSTANCE_HOST_DEPLOY/$INSTANCE_NAME-core"
 ./destroy.sh $GROUP "$INSTANCE_NAME-core"
@@ -52,13 +52,16 @@ kubectl delete pvc --namespace $GROUP data-$INSTANCE_NAME-database-postgresql-0
 INSTANCE_NAME=test-db-preset
 ./deploy-dhis2-db-preset.sh $GROUP $INSTANCE_NAME-preset
 ./deploy-dhis2-db-from-preset.sh whoami $INSTANCE_NAME $INSTANCE_NAME-preset
-./deploy-dhis2-core.sh $GROUP $INSTANCE_NAME $INSTANCE_NAME-core
+./deploy-dhis2-core.sh $GROUP $INSTANCE_NAME-core $INSTANCE_NAME
 sleep 3
-kubectl rollout status --watch --timeout=300s --namespace $GROUP statefulset/$INSTANCE_NAME-database-postgresql
-kubectl wait --for=condition=available --timeout=300s --namespace $GROUP deployment/$INSTANCE_NAME-core
+kubectl rollout status --watch --timeout=600s --namespace $GROUP statefulset/$INSTANCE_NAME-database-postgresql
+kubectl wait --for=condition=available --timeout=600s --namespace $GROUP "deployment/$INSTANCE_NAME-core"
 sleep 3
 http --check-status --follow "$INSTANCE_HOST_DEPLOY/$INSTANCE_NAME-core"
-./destroy.sh $GROUP $INSTANCE_NAME-core $INSTANCE_NAME-preset $INSTANCE_NAME
+#./destroy.sh $GROUP $INSTANCE_NAME-core $INSTANCE_NAME $INSTANCE_NAME-preset
+./destroy.sh $GROUP $INSTANCE_NAME-core
+./destroy.sh $GROUP $INSTANCE_NAME
+./destroy.sh $GROUP $INSTANCE_NAME-preset
 kubectl delete pvc --namespace $GROUP data-$INSTANCE_NAME-database-postgresql-0
 
 # Database and core from preset
@@ -67,9 +70,12 @@ INSTANCE_NAME=test-db-and-core-preset
 ./deploy-dhis2-preset.sh $GROUP $INSTANCE_NAME-preset
 ./deploy-dhis2-from-preset.sh $GROUP $INSTANCE_NAME $INSTANCE_NAME-preset
 sleep 3
-kubectl rollout status --watch --timeout=300s --namespace $GROUP statefulset/$INSTANCE_NAME-db-database-postgresql
-kubectl wait --for=condition=available --timeout=300s --namespace $GROUP deployment/$INSTANCE_NAME
+kubectl rollout status --watch --timeout=600s --namespace $GROUP statefulset/$INSTANCE_NAME-db-database-postgresql
+kubectl wait --for=condition=available --timeout=600s --namespace $GROUP deployment/$INSTANCE_NAME
 sleep 3
 http --check-status --follow "$INSTANCE_HOST_DEPLOY/$INSTANCE_NAME"
-./destroy.sh $GROUP $INSTANCE_NAME-db $INSTANCE_NAME-preset $INSTANCE_NAME
+#./destroy.sh $GROUP $INSTANCE_NAME-db $INSTANCE_NAME-preset $INSTANCE_NAME
+./destroy.sh $GROUP $INSTANCE_NAME
+./destroy.sh $GROUP $INSTANCE_NAME-preset
+./destroy.sh $GROUP $INSTANCE_NAME-db
 kubectl delete pvc --namespace $GROUP data-$INSTANCE_NAME-db-database-postgresql-0
