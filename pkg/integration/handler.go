be0948f72b1105bbfeba8def9b22aa07f2225e65
TonsV2 <sebastianthegreatful@gmail.com>
2022-12-15 09:46:48 +0700

    chore: swap methods, so they're declared in the order they're used

--- pkg/integration/handler.go
+++ pkg/integration/handler.go
@@ -145,34 +145,6 @@ func (h Handler) Integrations(c *gin.Context) {
 	c.Status(http.StatusNotFound)
 }
 
-func getInstances(token string, url string) (map[uint]string, error) {
-	b, err := httpGet(token, url)
-	if err != nil {
-		return nil, err
-	}
-
-	var groups []struct {
-		Name      string
-		Instances []struct {
-			ID   uint
-			Name string
-		}
-	}
-	err = json.Unmarshal(b, &groups)
-	if err != nil {
-		return nil, err
-	}
-
-	instances := make(map[uint]string)
-	for _, group := range groups {
-		for _, instance := range group.Instances {
-			instances[instance.ID] = fmt.Sprintf("%s/%s", group.Name, instance.Name)
-		}
-	}
-
-	return instances, nil
-}
-
 func getDatabases(token string, url string) (map[uint]string, error) {
 	b, err := httpGet(token, url)
 	if err != nil {
@@ -199,6 +171,34 @@ func getDatabases(token string, url string) (map[uint]string, error) {
 	}
 
 	return databases, nil
+}
+
+func getInstances(token string, url string) (map[uint]string, error) {
+	b, err := httpGet(token, url)
+	if err != nil {
+		return nil, err
+	}
+
+	var groups []struct {
+		Name      string
+		Instances []struct {
+			ID   uint
+			Name string
+		}
+	}
+	err = json.Unmarshal(b, &groups)
+	if err != nil {
+		return nil, err
+	}
+
+	instances := make(map[uint]string)
+	for _, group := range groups {
+		for _, instance := range group.Instances {
+			instances[instance.ID] = fmt.Sprintf("%s/%s", group.Name, instance.Name)
+		}
+	}
+
+	return instances, nil
 }
 
 func httpGet(token string, url string) ([]byte, error) {
