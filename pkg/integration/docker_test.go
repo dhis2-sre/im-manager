dcb66415a368cdb63c3e9b905b1e989702a35109
TonsV2 <sebastianthegreatful@gmail.com>
2022-12-18 14:28:05 +0700

    chore: change order of tests for the sake of readability

--- pkg/integration/docker_test.go
+++ pkg/integration/docker_test.go
@@ -13,6 +13,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_dockerHubClient_GetImages(t *testing.T) {
+	hubClient := NewDockerHubClient("", "")
+	hubClient.client = &clientMockImages{}
+
+	organization := "dhis2"
+	images, err := hubClient.GetImages(organization)
+	assert.NoError(t, err)
+
+	assert.True(t, slices.Contains(images, "core"))
+	assert.True(t, slices.Contains(images, "core-dev"))
+}
+
 type clientMockImages struct {
 }
 
@@ -37,16 +49,17 @@ func (c *clientMockImages) Do(*http.Request) (*http.Response, error) {
 	}, nil
 }
 
-func Test_dockerHubClient_GetImages(t *testing.T) {
+func Test_dockerHubClient_GetTags(t *testing.T) {
 	hubClient := NewDockerHubClient("", "")
-	hubClient.client = &clientMockImages{}
+	hubClient.client = &clientMockTags{}
 
 	organization := "dhis2"
 	images, err := hubClient.GetImages(organization)
 	assert.NoError(t, err)
 
-	assert.True(t, slices.Contains(images, "core"))
-	assert.True(t, slices.Contains(images, "core-dev"))
+	assert.True(t, slices.Contains(images, "2.39.0"))
+	assert.True(t, slices.Contains(images, "2.38.0"))
+	assert.True(t, slices.Contains(images, "2.37.0"))
 }
 
 type clientMockTags struct {
@@ -74,17 +87,4 @@ func (c *clientMockTags) Do(*http.Request) (*http.Response, error) {
 	return &http.Response{
 		Body: io.NopCloser(reader),
 	}, nil
-}
-
-func Test_dockerHubClient_GetTags(t *testing.T) {
-	hubClient := NewDockerHubClient("", "")
-	hubClient.client = &clientMockTags{}
-
-	organization := "dhis2"
-	images, err := hubClient.GetImages(organization)
-	assert.NoError(t, err)
-
-	assert.True(t, slices.Contains(images, "2.39.0"))
-	assert.True(t, slices.Contains(images, "2.38.0"))
-	assert.True(t, slices.Contains(images, "2.37.0"))
 }
