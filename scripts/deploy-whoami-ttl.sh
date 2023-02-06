31e366f9cdd912a4256af9cc29b7fcd8fd711569
TonsV2 <sebastianthegreatful@gmail.com>
2023-02-06 14:17:44 +0800

    chore: label stack pods with creation timestamp

---
+++ scripts/deploy-whoami-ttl.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source ./auth.sh
+
+STACK=whoami-go
+
+GROUP=$1
+NAME=$2
+TTL=${3:-""}
+CHART_VERSION=${CHART_VERSION:-0.9.0}
+IMAGE_TAG=${IMAGE_TAG:-0.6.0}
+
+echo "{
+  \"name\": \"$NAME\",
+  \"groupName\": \"$GROUP\",
+  \"stackName\": \"$STACK\",
+  \"optionalParameters\": [
+    {
+      \"name\": \"CHART_VERSION\",
+      \"value\": \"$CHART_VERSION\"
+    },
+    {
+      \"name\": \"IMAGE_TAG\",
+      \"value\": \"$IMAGE_TAG\"
+    },
+    {
+      \"name\": \"INSTANCE_TTL\",
+      \"value\": \"$TTL\"
+    }
+  ]
+}" | $HTTP post "$INSTANCE_HOST/instances" "Authorization: Bearer $ACCESS_TOKEN"
