1531d9e660e9701309427b40599e4147abf085e3
TonsV2 <sebastianthegreatful@gmail.com>
2023-02-10 17:44:02 +0800

    chore: remove login script. It's no longer used as login is automated

--- scripts/login.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-ACCESS_TOKEN=$($HTTP --auth "$USER_EMAIL:$PASSWORD" post "$IM_HOST/tokens" | jq -r '.access_token')
-
-echo "export ACCESS_TOKEN=$ACCESS_TOKEN"
