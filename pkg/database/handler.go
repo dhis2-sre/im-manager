72f9f1c8a1080298a190b49ec4f0df3188106aee
TonsV2 <sebastianthegreatful@gmail.com>
2023-05-26 17:15:57 +0800

    chore: don't use apperror

--- pkg/database/handler.go
+++ pkg/database/handler.go
@@ -254,8 +254,7 @@ func (h Handler) Save(c *gin.Context) {
 	instanceIdParam := c.Param("instanceId")
 	instanceId, err := strconv.ParseUint(instanceIdParam, 10, 32)
 	if err != nil {
-		badRequest := apperror.NewBadRequest("error parsing instanceId")
-		_ = c.Error(badRequest)
+		_ = c.AbortWithError(http.StatusBadRequest, errors.New("error parsing instanceId"))
 		return
 	}
 
