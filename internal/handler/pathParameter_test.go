614d3f0ca53450479c946533ea639cba4d12d910
TonsV2 <sebastianthegreatful@gmail.com>
2023-05-30 16:29:48 +0800

    test: add tests for handler.GetPathParameter

---
+++ internal/handler/pathParameter_test.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetPathParameter(t *testing.T) {
+	w := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(w)
+	ctx.AddParam("id", "123")
+
+	id, ok := GetPathParameter(ctx, "id")
+	assert.True(t, ok)
+	assert.Equal(t, uint(123), id)
+}
+
+func TestGetPathParameter_NotFound(t *testing.T) {
+	w := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(w)
+
+	id, ok := GetPathParameter(ctx, "id")
+	assert.False(t, ok)
+	assert.Equal(t, 0, id)
+}
