ca54c07a025cbf4600fe66a8becc7ad8d4cd5581
TonsV2 <sebastianthegreatful@gmail.com>
2023-06-05 12:36:27 +0800

    chore: use $ENVIRONMENT as part of access token cache filename

---
+++ scripts/README.md
@@ -0,0 +1,6 @@
+```sh
+cp .env.example .env
+# Fill in your credentials
+cd databases/ && ln -s ../.env .env
+# Do the same for users/ and instances/
+```
--- scripts/instances/auth.sh
+++ scripts/instances/auth.sh
@@ -2,12 +2,13 @@
 
 set -euo pipefail
 
-touch ./.access_token_cache
-ACCESS_TOKEN="$(cat ./.access_token_cache || "")"
+CACHE_FILE=./.access_token_cache_$ENVIRONMENT
+touch "$CACHE_FILE"
+ACCESS_TOKEN="$(cat "$CACHE_FILE" || "")"
 exp=$(echo "$ACCESS_TOKEN" | jq -R 'split(".")? | .[1] | @base64d | fromjson | .exp')
 NOW=$(date +%s)
 if [[ -z "$exp" ]] || (( $exp < $NOW )); then
   # shellcheck disable=SC2155
   export ACCESS_TOKEN=$($HTTP --auth "$USER_EMAIL:$PASSWORD" post "$IM_HOST/tokens" | jq -r '.access_token')
-  echo "$ACCESS_TOKEN" > ./.access_token_cache
+  echo "$ACCESS_TOKEN" > "$CACHE_FILE"
 fi
