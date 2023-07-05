fd1546900816944d277232de432064376246ad2d
TonsV2 <sebastianthegreatful@gmail.com>
2023-07-05 18:15:12 +0800

    refactor: join group with instance

--- pkg/instance/repository.go
+++ pkg/instance/repository.go
@@ -93,6 +93,7 @@ func (r repository) FindById(id uint) (*model.Instance, error) {
 	err := r.db.
 		Preload("RequiredParameters.StackRequiredParameter").
 		Preload("OptionalParameters.StackOptionalParameter").
+		Joins("Group").
 		First(&instance, id).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil, errdef.NewNotFound("instance not found by id: %d", id)
