b4a0b437560562f5d01bd9c858868a1687e03b72
radnov <radnov@pm.me>
2023-07-19 12:50:49 +0300

    fix: remove "required" binding from Deployable bool field
    If the Deployable bool field is `false`, the validation from the "required" tag will fail,
    as it's considered the zero-value for bools. In our case both `true` and `false`
    are valid, so there's no need to use the "required" tag.
    
    https://github.com/go-playground/validator/issues/713

--- scripts/users/createGroup.sh
+++ scripts/users/createGroup.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 
 GROUP=$1
 HOSTNAME=$2
+DEPLOYABLE=${3:-false}
 
 echo "{
   \"name\": \"$GROUP\",
-  \"hostname\": \"$HOSTNAME\"
+  \"hostname\": \"$HOSTNAME\",
+  \"deployable\": $DEPLOYABLE
 }" | $HTTP post "$IM_HOST/groups" "Authorization: Bearer $ACCESS_TOKEN"
