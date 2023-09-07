245b58a9dcdcd27db366212cc478d1d6d892f71a
teleivo <teleivo@users.noreply.github.com>
2023-09-07 12:03:44 +0200

    Revert "fix: swagger definition for stack"
    This reverts commit bf1a919e711c2c2b89f6d63b3e8328a591f063f0.

--- pkg/model/stack.go
+++ pkg/model/stack.go
@@ -1,25 +1,26 @@
 package model
 
+// swagger:model Stack
 type Stack struct {
-	Name             string
-	Parameters       StackParameters
-	Instances        []Instance
-	HostnamePattern  string
-	HostnameVariable string
+	Name             string          `json:"name"`
+	Parameters       StackParameters `json:"parameters"`
+	Instances        []Instance      `json:"instances"`
+	HostnamePattern  string          `json:"hostnamePattern"`
+	HostnameVariable string          `json:"hostnameVariable"`
 	// ParameterProviders provide parameters to other stacks.
-	ParameterProviders ParameterProviders
+	ParameterProviders ParameterProviders `json:"-"`
 	// Requires these stacks to deploy an instance of this stack.
-	Requires []Stack
+	Requires []Stack `json:"-"`
 }
 
 type StackParameters map[string]StackParameter
 
 type StackParameter struct {
-	DefaultValue *string
+	DefaultValue *string `json:"defaultValue,omitempty"`
 	// Consumed signals that this parameter is provided by another stack i.e. one of the stacks required stacks.
-	Consumed bool
+	Consumed bool `json:"consumed"`
 	// Validator ensures that the actual stack parameters are valid according to its rules.
-	Validator func(value string) error
+	Validator func(value string) error `json:"-"`
 }
 
 type ParameterProviders map[string]ParameterProvider
