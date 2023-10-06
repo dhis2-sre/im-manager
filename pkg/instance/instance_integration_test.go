29ec8d1424b2190283581dfea6fd48d9d92e9b30
TonsV2 <sebastianthegreatful@gmail.com>
2023-10-06 09:49:40 +0700

    test: deploy DHIS2. Works locally but not on GHA!

--- pkg/instance/instance_integration_test.go
+++ pkg/instance/instance_integration_test.go
@@ -1,10 +1,15 @@
 package instance_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -103,6 +108,53 @@ func TestInstanceHandler(t *testing.T) {
 		client.PostJSON(t, "/instances", body, &instance, inttest.WithAuthToken("sometoken"))
 
 		k8sClient.AssertPodIsReady(t, group.Name, instance.Name, 60)
+	})
+
+	var databaseID string
+	{
+		t.Log("Upload")
+		var b bytes.Buffer
+		w := multipart.NewWriter(&b)
+		err := w.WriteField("group", "group-name")
+		require.NoError(t, err, "failed to write form field")
+		f, err := w.CreateFormFile("database", "mydb")
+		require.NoError(t, err, "failed to create form file")
+		_, err = io.WriteString(f, "select now();")
+		require.NoError(t, err, "failed to write file")
+		_ = w.Close()
+
+		body := client.Post(t, "/databases", &b, inttest.WithHeader("Content-Type", w.FormDataContentType()))
+
+		var actualDB model.Database
+		err = json.Unmarshal(body, &actualDB)
+		require.NoError(t, err, "POST /databases: failed to unmarshal HTTP response body")
+		require.Equal(t, "mydb", actualDB.Name)
+		require.Equal(t, "group-name", actualDB.GroupName)
+
+		databaseID = strconv.FormatUint(uint64(actualDB.ID), 10)
+	}
+
+	t.Run("DeployDHIS2", func(t *testing.T) {
+		hostname := client.GetHostname(t)
+		t.Log("hostname:", hostname)
+		t.Setenv("HOSTNAME", hostname)
+
+		var instance model.Instance
+		body := strings.NewReader(`{
+			"name": "test-dhis2",
+			"groupName": "group-name",
+			"stackName": "dhis2",
+			"parameters": [
+                {
+    		        "name": "DATABASE_ID",
+			        "value": "` + databaseID + `"
+			    }
+			]
+		}`)
+		client.PostJSON(t, "/instances", body, &instance, inttest.WithAuthToken("sometoken"))
+
+		k8sClient.AssertPodIsReady(t, group.Name, instance.Name+"-database", 60)
+		k8sClient.AssertPodIsReady(t, group.Name, instance.Name, 5*60)
 	})
 
 	t.Run("DeployDeploymentWithoutInstances", func(t *testing.T) {
