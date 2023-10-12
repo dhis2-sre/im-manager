418077aaf23127b25757174b68d6e02acb52b92b
radnov <radnov@pm.me>
2023-10-12 11:48:29 +0300

    refactor: bump default dhis2 and dhis2-db stacks DATABASE_SIZE (#517)
    5Gi is not enough to generate analytics for the SL Demo DB.
    With analytics generated, the SL Demo database is about 15GB.

--- scripts/instances/create-deployment-instance-dhis2-db.sh
+++ scripts/instances/create-deployment-instance-dhis2-db.sh
@@ -8,7 +8,7 @@ DEPLOYMENT_ID=$1
 STACK_NAME=dhis2-db
 
 DATABASE_ID=${DATABASE_ID:-whoami-sierra-leone-2-40-0-sql-gz}
-DATABASE_SIZE=${DATABASE_SIZE:-5Gi}
+DATABASE_SIZE=${DATABASE_SIZE:-20Gi}
 
 echo "{
   \"stackName\": \"$STACK_NAME\",
