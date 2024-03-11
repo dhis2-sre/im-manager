7a13dc2c33bb20d72289e480c11e65b38f35ddf0
tonsV2 <tonsV2@users.noreply.github.com>
2024-03-11 16:14:08 +0800

    chore: add user scripts for bulk deployment of DHIS2 and invoking of analytics generation (#709)

---
+++ scripts/instances/bulk-dhis2-deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+GROUP=$1
+INSTANCES=$2
+NAME=$3
+
+export STARTUP_PROBE_PERIOD_SECONDS=10
+
+export DATABASE_ID=test-dbs-sierra-leone-dev-sql-gz
+export DATABASE_SIZE=20Gi
+
+export IMAGE_REPOSITORY=core-dev
+#export IMAGE_PULL_POLICY=Always
+export IMAGE_TAG=2.41
+
+export CORE_RESOURCES_REQUESTS_CPU=500m # 250m
+export CORE_RESOURCES_REQUESTS_MEMORY=2500Mi # 1500Mi
+
+
+for ((i = 0; i < INSTANCES; i++)); do
+  # Ensure each deploy get a fresh access token
+  rm -f .access_token_cache
+  source ./auth.sh
+  ./deploy-dhis2.sh "$GROUP" "$NAME-$i"
+done
