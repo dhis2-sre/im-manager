452517d27b8f6691dc599168beca5669507bf9a2
Andreas Jensen <andreas@dhis2.org>
2024-05-20 01:49:15 +0800

    chore: declare RESPONSE closer to where it's used

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -8,9 +8,8 @@ touch ./.access_token_cache
 ACCESS_TOKEN="$(cat ./.access_token_cache || "")"
 exp=$(echo "$ACCESS_TOKEN" | jq -R 'split(".")? | .[1] | @base64d | fromjson | .exp')
 NOW=$(date +%s)
-RESPONSE=""
 if [[ -z "$exp" ]] || (( exp < NOW )); then
-  # shellcheck disable=SC2155
+  RESPONSE=""
   if [[ -z "$IM_USER_TYPE" ]]; then
     RESPONSE=$(echo "{}" | $HTTP --headers --auth "$USER_EMAIL:$PASSWORD" post "$IM_HOST/tokens")
   else
