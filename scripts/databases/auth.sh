2c9ff547e27b88eaf1c0352e2d08d1b70441bcd4
Andreas Jensen <andreas@dhis2.org>
2024-05-20 01:09:36 +0800

    chore: symlink auth.sh

--- scripts/databases/auth.sh
+++ scripts/databases/auth.sh
@@ -1,13 +1,1 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-touch ./.access_token_cache
-ACCESS_TOKEN="$(cat ./.access_token_cache || "")"
-exp=$(echo "$ACCESS_TOKEN" | jq -R 'split(".")? | .[1] | @base64d | fromjson | .exp')
-NOW=$(date +%s)
-if [[ -z "$exp" ]] || (( exp < NOW )); then
-  # shellcheck disable=SC2155
-  export ACCESS_TOKEN=$($HTTP --auth "$USER_EMAIL:$PASSWORD" post "$IM_HOST/tokens" | jq -r '.accessToken')
-  echo "$ACCESS_TOKEN" > ./.access_token_cache
-fi
+../auth.sh
