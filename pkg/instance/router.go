e6d65bc4ea646b97bc156b582d1f901983dae01a
tonsV2 <tonsV2@users.noreply.github.com>
2024-07-09 15:05:19 +0200

    feat: list public deployments (#813)
    * feat: list public deployments
    
    * chore: fix names
    
    * chore: use correct return type
    
    * chore: update doc
    
    * chore: correct swagger identifier
    
    * chore: return custom error
    
    * chore: explicitly return nil error
    
    * chore: swagger
    
    * chore: run tests in parallel
    
    * chore: add whitespace to indicate given, when, then structure
    
    * chore: run tests in parallel

--- pkg/instance/router.go
+++ pkg/instance/router.go
@@ -5,6 +5,8 @@ import (
 )
 
 func Routes(r *gin.Engine, authenticator gin.HandlerFunc, handler Handler) {
+	r.GET("/deployments/public", handler.FindPublicDeployments)
+
 	tokenAuthenticationRouter := r.Group("")
 	tokenAuthenticationRouter.Use(authenticator)
 
