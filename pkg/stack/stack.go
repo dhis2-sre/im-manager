7cbd9262908874474ab2a83becad353c3af6589c
Andreas Jensen <andreas@dhis2.org>
2025-07-09 18:38:43 +0300

    feat: support DHIS2 with Embedded Tomcat

--- pkg/stack/stack.go
+++ pkg/stack/stack.go
@@ -257,7 +257,7 @@ var dhis2CoreDefaults = struct {
 	googleAuthClientEmail        string
 	googleAuthClientId           string
 }{
-	chartVersion:                 "0.21.0",
+	chartVersion:                 "0.24.0",
 	minIOChartVersion:            "14.7.5",
 	minIOStorageSize:             "8Gi",
 	storageType:                  minIOStorage,
