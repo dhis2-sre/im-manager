268d951eb3ccb5d31b736cedd4f6b85bf8dc5b11
Andreas Jensen <andreas@dhis2.org>
2025-08-04 17:05:31 +0200

    chore: imports

--- pkg/inttest/k8s.go
+++ pkg/inttest/k8s.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/dhis2-sre/im-manager/pkg/model"
+
 	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
