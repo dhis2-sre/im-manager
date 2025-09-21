c70fb0e39e46adb4998fb9d2fd7c7e18511c99bb
Andreas Jensen <andreas@dhis2.org>
2025-09-22 02:16:52 +0700

    chore: show all extensions

--- init-scripts/01-create-extensions.sh
+++ init-scripts/01-create-extensions.sh
@@ -3,3 +3,5 @@ set -e
 
 psql -v -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
 psql -v -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "SELECT extversion FROM pg_extension WHERE extname = 'pg_trgm';"
+
+psql -v -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "SELECT * FROM pg_extension;"
