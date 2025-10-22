b03de3582b6bd04654619c7b7b7f3f27455532b7
Rado <radoslav@dhis2.org>
2025-10-22 18:29:21 +0300

    test: update ttl destroy handler test
    Signed-off-by: Rado <radoslav@dhis2.org>

--- pkg/inspector/ttlDestroyHandler_test.go
+++ pkg/inspector/ttlDestroyHandler_test.go
@@ -30,11 +30,18 @@ func Test_TTLDestroyHandler_NotExpired(t *testing.T) {
 func Test_TTLDestroyHandler_Expired(t *testing.T) {
 	ctx := context.TODO()
 	deployment := model.Deployment{
+		ID:        123,
 		CreatedAt: time.Now().Add(time.Minute * -10),
 		TTL:       300,
 	}
+	decryptedDeployment := &model.Deployment{
+		ID:        123,
+		CreatedAt: deployment.CreatedAt,
+		TTL:       300,
+	}
 	instanceService := &mockInstanceService{}
-	instanceService.On("DeleteDeployment", ctx, &deployment).Return(nil)
+	instanceService.On("FindDecryptedDeploymentById", ctx, deployment.ID).Return(decryptedDeployment, nil)
+	instanceService.On("DeleteDeployment", ctx, decryptedDeployment).Return(nil)
 
 	handler := NewTTLDestroyHandler(slog.Default(), instanceService)
 
@@ -49,4 +56,9 @@ type mockInstanceService struct{ mock.Mock }
 func (m *mockInstanceService) DeleteDeployment(ctx context.Context, deployment *model.Deployment) error {
 	called := m.Called(ctx, deployment)
 	return called.Error(0)
+}
+
+func (m *mockInstanceService) FindDecryptedDeploymentById(ctx context.Context, id uint) (*model.Deployment, error) {
+	called := m.Called(ctx, id)
+	return called.Get(0).(*model.Deployment), called.Error(1)
 }
