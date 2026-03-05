6d7c331c4a8b5ef0a7b740aa0b6c6977b10b3bc5
Rado <radoslav@dhis2.org>
2026-03-05 13:01:14 +0200

    test: extract SaveAs setup into saveAsServiceFixture
    Signed-off-by: Rado <radoslav@dhis2.org>

--- pkg/database/service_test.go
+++ pkg/database/service_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/dhis2-sre/im-manager/pkg/storage"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"gorm.io/gorm"
 )
 
 func TestDatabaseService(t *testing.T) {
@@ -22,71 +23,20 @@ func TestDatabaseService(t *testing.T) {
 	t.Run("SaveAs", func(t *testing.T) {
 		t.Parallel()
 
-		db := inttest.SetupDB(t)
-		logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-
-		s3Spy := &s3ClientSpy{}
-
-		group := &model.Group{
-			Name:      "packages",
-			Hostname:  "some",
-			Namespace: "ns",
-		}
-		user := &model.User{
-			Email:  "user1@dhis2.org",
-			Groups: []model.Group{*group},
-		}
-		db.Create(user)
-
-		sourceDB := &model.Database{
-			Name:      "source.sql.gz",
-			GroupName: "packages",
-			Type:      "database",
-			Url:       "s3://database-bucket/packages/source.sql.gz",
-			UserID:    user.ID,
-		}
-		db.Create(sourceDB)
-
-		repo := NewRepository(db)
-
-		fakeGroup := &mockGroupService{group: group}
-		fakePod := &fakePodExec{output: []byte("pg_dump output")}
-		podExecFunc := podExecutorFunc(func(_ model.Cluster) (PodExecutor, error) {
-			return fakePod, nil
-		})
-
-		svc := NewService(logger, "database-bucket", s3Spy, fakeGroup, repo, podExecFunc)
-
-		instance := &model.DeploymentInstance{
-			Name:      "name",
-			GroupName: "packages",
-			StackName: "dhis2-db",
-			Group:     group,
-			Parameters: model.DeploymentInstanceParameters{
-				"DATABASE_ID":       {Value: "1"},
-				"DATABASE_NAME":     {Value: "dhis2"},
-				"DATABASE_USERNAME": {Value: "dhis"},
-				"DATABASE_PASSWORD": {Value: "dhis"},
-			},
-		}
-
-		stack := &model.Stack{
-			Name:            "dhis2-db",
-			HostnamePattern: "%s-database-postgresql.%s.svc",
-		}
+		fx := setupSaveAsServiceFixture(t)
 
 		doneCh := make(chan *model.Database, 1)
 		done := func(_ context.Context, saved *model.Database) {
 			doneCh <- saved
 		}
 
-		newDB, err := svc.SaveAs(context.Background(), user.ID, sourceDB, instance, stack, "new-save.sql.gz", "custom", done)
+		newDB, err := fx.Svc.SaveAs(context.Background(), fx.User.ID, fx.SourceDB, fx.Instance, fx.Stack, "new-save.sql.gz", "custom", done)
 		require.NoError(t, err)
 
 		assert.Equal(t, "new-save.sql.gz", newDB.Name)
 		assert.Equal(t, "packages", newDB.GroupName)
 		assert.Equal(t, "database", newDB.Type)
-		assert.Equal(t, user.ID, newDB.UserID)
+		assert.Equal(t, fx.User.ID, newDB.UserID)
 		assert.NotZero(t, newDB.ID)
 
 		select {
@@ -97,16 +47,94 @@ func TestDatabaseService(t *testing.T) {
 			t.Fatal("timed out waiting for async SaveAs to complete")
 		}
 
-		s3Spy.mu.Lock()
-		assert.Equal(t, "database-bucket", s3Spy.lastBucket)
-		assert.Equal(t, "packages/new-save.sql.gz", s3Spy.lastKey)
-		s3Spy.mu.Unlock()
+		fx.S3Spy.mu.Lock()
+		assert.Equal(t, "database-bucket", fx.S3Spy.lastBucket)
+		assert.Equal(t, "packages/new-save.sql.gz", fx.S3Spy.lastKey)
+		fx.S3Spy.mu.Unlock()
 
 		var finalDB model.Database
-		db.First(&finalDB, newDB.ID)
+		err = fx.DB.First(&finalDB, newDB.ID).Error
+		require.NoError(t, err)
 		assert.Equal(t, "s3://database-bucket/packages/new-save.sql.gz", finalDB.Url)
 		assert.Greater(t, finalDB.Size, int64(0))
 	})
+}
+
+type saveAsServiceFixture struct {
+	DB       *gorm.DB
+	Svc      *service
+	User     *model.User
+	SourceDB *model.Database
+	Instance *model.DeploymentInstance
+	Stack    *model.Stack
+	S3Spy    *s3ClientSpy
+}
+
+func setupSaveAsServiceFixture(t *testing.T) saveAsServiceFixture {
+	t.Helper()
+
+	db := inttest.SetupDB(t)
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	s3Spy := &s3ClientSpy{}
+
+	group := &model.Group{
+		Name:      "packages",
+		Hostname:  "some",
+		Namespace: "ns",
+	}
+	user := &model.User{
+		Email:  "user1@dhis2.org",
+		Groups: []model.Group{*group},
+	}
+	db.Create(user)
+
+	sourceDB := &model.Database{
+		Name:      "source.sql.gz",
+		GroupName: "packages",
+		Type:      "database",
+		Url:       "s3://database-bucket/packages/source.sql.gz",
+		UserID:    user.ID,
+	}
+	db.Create(sourceDB)
+
+	repo := NewRepository(db)
+
+	fakeGroup := &mockGroupService{group: group}
+	fakePod := &fakePodExec{output: []byte("pg_dump output")}
+	podExecFunc := podExecutorFunc(func(_ model.Cluster) (PodExecutor, error) {
+		return fakePod, nil
+	})
+
+	svc := NewService(logger, "database-bucket", s3Spy, fakeGroup, repo, podExecFunc)
+
+	instance := &model.DeploymentInstance{
+		Name:      "name",
+		GroupName: "packages",
+		StackName: "dhis2-db",
+		Group:     group,
+		Parameters: model.DeploymentInstanceParameters{
+			"DATABASE_ID":       {Value: "1"},
+			"DATABASE_NAME":     {Value: "dhis2"},
+			"DATABASE_USERNAME": {Value: "dhis"},
+			"DATABASE_PASSWORD": {Value: "dhis"},
+		},
+	}
+
+	stack := &model.Stack{
+		Name:            "dhis2-db",
+		HostnamePattern: "%s-database-postgresql.%s.svc",
+	}
+
+	return saveAsServiceFixture{
+		DB:       db,
+		Svc:      svc,
+		User:     user,
+		SourceDB: sourceDB,
+		Instance: instance,
+		Stack:    stack,
+		S3Spy:    s3Spy,
+	}
 }
 
 type mockGroupService struct {
