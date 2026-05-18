a3fcbc9347e52d50d3f3d814997af6b24d708764
tonsV2 <tonsV2@users.noreply.github.com>
2026-05-18 15:08:41 +0700

    fix: align admin user update password minimum to 24 characters (#1505)

--- pkg/user/handler.go
+++ pkg/user/handler.go
@@ -549,7 +549,7 @@ func (h Handler) Delete(c *gin.Context) {
 
 type updateUserRequest struct {
 	Email    string `json:"email" binding:"omitempty,email"`
-	Password string `json:"password" binding:"omitempty,gte=16,lte=128"`
+	Password string `json:"password" binding:"omitempty,gte=24,lte=128"`
 }
 
 // Update user
